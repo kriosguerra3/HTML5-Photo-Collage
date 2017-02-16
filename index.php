ab10e156b63410648232fbf3b82c67e942539af4
Kelly Rios <kriosguerra@gmail.com>
2017-02-16 13:32:25 -0600

    Second commit
    Basic HTML file structure

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	</head>
+<body>
 <?
 	echo "Hola PHP!";
 ?>
+</body>
+</html>
