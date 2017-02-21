137d1f52ecbb1b98f050d1e40b2ee23c9d83edd2
Kelly Rios <kriosguerra@gmail.com>
2017-02-21 17:21:27 -0600

    Layouts ids updated to classes
    In order to reuse the CSS properties of the layout examples, all
    layouts ids are updated to classes

--- index.php
+++ index.php
@@ -7,63 +7,69 @@
 		<button type="button" id="link_group_<? echo $i ?>" class="btn btn-lg btn-link" ><? echo $i ?></button>
 	<? } ?>
 
-	
-	<!-- 2 photos-->
-	<div id= "group_2" class="row layout_group">
-		
-		<div id="lay_21" class="photo_layout_example">
-			<div id="lay_21_1"></div>
-			<div id="lay_21_2"></div>
-		</div>	
+	<div id="layout_group_container">
+		<!-- 2 photos-->
+		<div id= "group_2" class="row layout_group">
+			
+			<div class="lay_21 photo_layout_example">
+				<div class="lay_21_1"></div>
+				<div class="lay_21_2"></div>
+			</div>	
 
-		<div id="lay_22" class="photo_layout_example">
-			<div id="lay_22_1"></div>
-			<div id="lay_22_2"></div>
-		</div>	
-		
+			<div class="lay_22 photo_layout_example">
+				<div class="lay_22_1"></div>
+				<div class="lay_22_2"></div>
+			</div>	
+			
+		</div>
+
+		<!-- 3 photos-->
+		<div id= "group_3" class="row layout_group" >
+			
+			<div class="lay_31 photo_layout_example">
+				<div class="lay_31_1"></div>
+				<div class="lay_31_2"></div>
+				<div class="lay_31_3"></div>
+			</div>
+
+			<div class="lay_32 photo_layout_example">
+				<div class="lay_32_1"></div>
+				<div class="lay_32_2"></div>
+				<div class="lay_32_3"></div>
+			</div>	
+
+			<div class="lay_33 photo_layout_example">
+				<div class="lay_33_1"></div>
+				<div class="lay_33_2"></div>
+				<div class="lay_33_3"></div>
+			</div>	
+
+			<div class="lay_34 photo_layout_example">
+				<div class="lay_34_1"></div>
+				<div class="lay_34_2"></div>
+				<div class="lay_34_3"></div>
+			</div>
+
+			<div class="lay_35 photo_layout_example">
+				<div class="lay_35_1"></div>
+				<div class="lay_35_2"></div>
+				<div class="lay_35_3"></div>
+			</div>	
+
+			<div class="lay_36 photo_layout_example">
+				<div class="lay_36_1"></div>
+				<div class="lay_36_2"></div>
+				<div class="lay_36_3"></div>
+			</div>
+		</div>
 	</div>
 
-	<!-- 3 photos-->
-	<div id= "group_3" class="row layout_group" >
-		
-		<div id="lay_31" class="photo_layout_example">
-			<div id="lay_31_1"></div>
-			<div id="lay_31_2"></div>
-			<div id="lay_31_3"></div>
-		</div>
-
-		<div id="lay_32" class="photo_layout_example">
-			<div id="lay_32_1"></div>
-			<div id="lay_32_2"></div>
-			<div id="lay_32_3"></div>
-		</div>	
-
-		<div id="lay_33" class="photo_layout_example">
-			<div id="lay_33_1"></div>
-			<div id="lay_33_2"></div>
-			<div id="lay_33_3"></div>
-		</div>	
-
-		<div id="lay_34" class="photo_layout_example">
-			<div id="lay_34_1"></div>
-			<div id="lay_34_2"></div>
-			<div id="lay_34_3"></div>
-		</div>
-
-		<div id="lay_35" class="photo_layout_example">
-			<div id="lay_35_1"></div>
-			<div id="lay_35_2"></div>
-			<div id="lay_35_3"></div>
-		</div>	
-
-		<div id="lay_36" class="photo_layout_example">
-			<div id="lay_36_1"></div>
-			<div id="lay_36_2"></div>
-			<div id="lay_36_3"></div>
-		</div>
-	
+	<div id="layout_main_container">
+		<div id="photo_layout_main"></div>
 	</div>
+	
 	<div class="button_container">
+		<button type="button" id="back" class="btn btn-primary">Previous</button>
 		<button type="button" id="next" class="btn btn-primary">Next</button>	
 	</div>	
 </div>
--- scripts.js
+++ scripts.js
@@ -4,18 +4,18 @@ $(function() {
 	//When clicking the numbered links... 
 	$( ".btn-link" ).click(function() {
 
+		///ADD VALIDATION IN CASE NO ONE IS SELECTED
+
 		//...we remove previously selected layouts
 		$(".photo_layout_example").removeClass('selected');
 
 		//...we form the id attribute of the layout group to display. We only need to remove the "link_"substring and add "#"
 		group_id = "#"+ $(this).attr("id").replace('link_','');
-		console.log(group_id);
 
 		//...fade out the other groups and displaying the selected one when finished
 		$("[id^=group_]").fadeOut( "fast", function() {
 			$(group_id).css("display","block");
 		});
-
 	});
 
 	//When selecting the layout, we color the border red
@@ -23,5 +23,28 @@ $(function() {
 		$(this).toggleClass("selected").siblings().removeClass('selected');
 	});
 
+	//When clicking the next button...
+	$( "#next" ).click(function() {
+		
+		//...we get the classes from the selected layout example.The class we need to get the layout is the first one of the list (with format "lay_##"). We use the split method to get it.		
+		classes_array = $('.selected').attr('class').split(" ");
 
+		//We get a class with a "lay_##" format
+		layout_class= classes_array[0];
+		
+		//We get the number of photos of the layout so we can dinamically build it on the next step.
+		total_photos = layout_class.charAt(4);
+		
+		//We append to the big layout we will be building our collage on the divs and classes from the selected layout
+		for (var i = 1; i <= total_photos; i++) {
+			$('#photo_layout_main').append( '<div class="' +layout_class + '_' + i +'"></div>');
+		};
+
+		$("#layout_group_container").css("display","none");
+		$("#layout_main_container").css("display","block");
+
+
+
+
+	});
 });
