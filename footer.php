1c86e9e28e749c6aeaefe9435c5aa5c00972d73e
Kelly Rios <kriosguerra@gmail.com>
2017-03-01 13:28:51 -0600

    Bootstrap carousel implemented
    Bootstrap carousel implemented for filters displaying

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 			<script src="//code.jquery.com/jquery-1.12.4.js"></script>
 	  	<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<script src="scripts.js"></script>
 </body>
 </html>
